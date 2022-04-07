1ed376c7d76bad1cf29b21add3a19b6f6075160a
Karri von Schalien <103138747+kvScepter@users.noreply.github.com>
2022-04-07 10:17:21 +0300

    Create data_sources.py

---
+++ Data analytics and Artificial Intelligence/data_sources.py
@@ -0,0 +1,17 @@
+from operator import itemgetter
+import json
+with open("./data files/gamedata.json") as gamedata:
+    pelit = json.load(gamedata)
+
+#print(type(pelit))
+
+#print(len(pelit))
+
+#for i in range(len(pelit))[:3]:
+   
+        #print("pelin: ",pelit[i].get("title"), " metacritit arvo: ",pelit[i].get("metacriticScore"))
+    
+pelit2 = sorted(pelit, key=itemgetter("metacriticScore"), reverse=True)
+pelit2 = pelit2 [:3]
+for d in pelit2:
+    print("Pelillä :", d["title"], " Metacritic arvo on :", d["metacriticScore"])
