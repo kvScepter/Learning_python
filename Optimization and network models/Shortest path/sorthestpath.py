f12dd4ca3bf0c7af4c823862c4fb4f911d0cfa2a
Karri von Schalien <103138747+kvScepter@users.noreply.github.com>
2022-04-07 12:13:07 +0300

    Create sorthestpath.py
    Sorthes path from csv file with distance as weight. Uses Dijkstra algorythm

---
+++ Optimization and network models/Shortest path/sorthestpath.py
@@ -0,0 +1,19 @@
+import numpy as np
+import networkx as nx
+import sys
+sys.path.append("./Lib")
+
+#csv tiedostossa oleva matriisi käyttöön
+numpy_matriisi = np.genfromtxt('distance.csv', delimiter=',')
+G = nx.from_numpy_array(numpy_matriisi)
+
+#Lyhin reitti ja etäisyys
+#ajettava algorytmi (mikä, mistä, mihin tässä 6 --> 4 noodiin) 
+reitti = nx.dijkstra_path(G, 6, 4)
+reitti_pituus = nx.dijkstra_path_length(G, 6, 4)
+
+
+#tulostaa tuloksen, reitin pituun kun matriisissa painoarvot
+# ja reitin mikä on lyhin reitti
+print(reitti_pituus)
+print(reitti)
