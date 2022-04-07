62c2ccae850519bc31cef000f2b5b40b6215bc2b
Karri von Schalien <103138747+kvScepter@users.noreply.github.com>
2022-04-07 13:57:58 +0300

    Update basic_mod1.py

--- Cryptography/basic_mod1.py
+++ Cryptography/basic_mod1.py
@@ -1,2 +1,20 @@
-with open("decrypthis.txt") as filp
+import string
+
+with open("decrypthis.txt") as filp:
+          
+          contents = filp.read()
+          #make list of numbers to list
+          numbers = [ int(value) for value in contents.split() ]
+          # print(numbers)
+          for number in numbers:
+                    modulus = number % 37
+                    
+                    if modulus in range(0, 26):
+                              print(string.ascii_letters[modulus], end="")
+                    elif:     modulus in range(26, 36):
+                              print(string.digits[modulus-26], end="")
+                    else:
+                              print("_", end="")
+                    
+         
           
