25bf95a780d137686d4e1941bc5e50ef95bcc457
Karri von Schalien <103138747+kvScepter@users.noreply.github.com>
2022-04-07 09:50:57 +0300

    Create python_fundamentals1.py

---
+++ Data analytics and Artificial Intelligence/python_fundamentals1.py
@@ -0,0 +1,11 @@
+# Sentence
+s = "Gary must look towards the future in order to gather enough information from the target."
+
+# print first ten and last ten characters concatenated
+print(s[:10], s[-10:])
+
+# Count the number of white spaces between words from the previous sentence and divide that number with the length of the sentence.
+number_of_whitespace = s.count(" ") / len(s) 
+print(number_of_whitespace)
+
+
