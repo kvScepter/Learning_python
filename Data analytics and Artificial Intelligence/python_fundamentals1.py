99e3194555cd682e7bcdab9eaef76aa789b3046b
Karri von Schalien <103138747+kvScepter@users.noreply.github.com>
2022-04-07 09:55:56 +0300

    Update python_fundamentals1.py

--- Data analytics and Artificial Intelligence/python_fundamentals1.py
+++ Data analytics and Artificial Intelligence/python_fundamentals1.py
@@ -8,4 +8,10 @@ print(s[:10], s[-10:])
 number_of_whitespace = s.count(" ") / len(s) 
 print(number_of_whitespace)
 
-
+# Convert the sentence presented below to list using commas (,) as separators. Then create a new list which contains only the last three items from the list.
+# Sentence
+s2 = "Occaecat short ribs incididunt laboris. Bresaola, brisket ex, landjaeger ad, boudin pariatur frankfurter eu flank. Enim tail short ribs, shoulder duis minim excepteur, deserunt lorem porchetta. Dolor chicken spare ribs, id in, ball, tip, hamburger labore venison consectetur cupidatat."
+lista = s2.split(" ")
+print(lista)
+uusi_lista = lista[-3:]
+print(uusi_lista)
