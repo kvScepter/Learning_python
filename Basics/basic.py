62db80ea5ab4161755333e5388c02afc3b07490d
Karri von Schalien <103138747+kvScepter@users.noreply.github.com>
2022-04-25 21:41:09 +0300

    Create basic.py

---
+++ Basics/basic.py
@@ -0,0 +1,21 @@
+# Sequential Steps
+x = 43
+print(x)
+x = x + 2
+print(x)
+
+# Conditional Steps
+x = 5
+if x < 10:
+    print('smaller')
+if x > 20:
+    print("bigger")
+
+print('ende')
+
+# Repeated Steps
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print('Blastoff!')
